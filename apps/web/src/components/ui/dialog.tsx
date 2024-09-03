5ca55edebcd4725d820a335a9adce5729021e216
Tomás Cichero <martoxdlol@gmail.com>
2024-09-03 20:37:16 -0300

    make dialog mobile friendly

--- apps/web/src/components/ui/dialog.tsx
+++ apps/web/src/components/ui/dialog.tsx
@@ -41,7 +41,7 @@ const DialogContent = React.forwardRef<
         <DialogPrimitive.Content
             ref={ref}
             className={cn(
-                'fixed left-[50%] top-[calc(calc(var(--screen-height)_/_2)_+_var(--scroll-margin-top))] z-50 grid max-h-[var(--screen-height)] w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
+                'fixed left-[50%] bottom-0 sm:bottom-auto sm:top-[calc(calc(var(--screen-height)_/_2)_+_var(--scroll-margin-top))] z-50 grid max-h-[var(--screen-height)] w-full max-w-lg translate-x-[-50%] sm:translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg',
                 className,
             )}
             {...props}
